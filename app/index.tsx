fb07a509de2f7e0e9345a6ce37400c620131b83a
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-06-25 16:12:59 +0530

    chore: some changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Client, Account, AppwriteException, OAuthProvider } from "appwrite";
+import { Client, Account, AppwriteException, OAuthProvider } from "react-native-appwrite";
 import { useEffect, useState } from "react";
 import {
   View,
@@ -12,6 +12,7 @@ import {
   StatusBar,
   KeyboardAvoidingView,
   ScrollView,
+  AppState,
 } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Linking from "expo-linking";
@@ -58,7 +59,7 @@ export default function AuthScreen() {
     try {
       const deepLink = Linking.createURL("/");
 
-      const response = await account.createOAuth2Session(
+      const response = await account.createOAuth2Token(
         OAuthProvider.Google,
         deepLink,
         deepLink
@@ -99,6 +100,23 @@ export default function AuthScreen() {
       }
     };
     fetchUser();
+  }, []);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", async (nextAppState) => {
+      if (nextAppState === "active") {
+        try {
+          const user = await account.get();
+          setUser(user);
+        } catch (err) {
+          setUser(null);
+        }
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
   }, []);
 
   if (checkingUser) {
